16a1e9a41ec5cffe27b4946ecf57bcc5eed81f53
naushadhb <naushadhb@yahoo.com>
2024-09-17 08:30:23 -0400

    added new hanlder file

---
+++ filter_1.py
@@ -0,0 +1,6 @@
+from snowflake.snowpark.functions import col
+
+def filter_by_territory(session,table_name,territory)
+  df = session.table(table_name)
+  return df.filter(col("territory") == territory)
+
