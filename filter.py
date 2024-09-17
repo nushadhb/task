5412574ca76b1f7b2e2d78053fe293d9930afdb2
naushadhb <naushadhb@yahoo.com>
2024-09-17 08:01:02 -0400

    added handler

--- filter.py
+++ filter.py
@@ -3,3 +3,8 @@ from snowflake.snowpark.functions import col
 def filter_by_role(session, table_name, role):
   df = session.table(table_name)
   return df.filter(col("role") == role)
+
+def filter_by_territory(session,table_name,territory)
+  df = session .table(table_name)
+  return df.filter(col('territory') == territory)
+
