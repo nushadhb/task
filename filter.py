7650410033ba0845f3f7b55e3890c4ef9f6173fb
naushadhb <naushadhb@yahoo.com>
2024-09-17 08:34:55 -0400

    added new hanlder file

--- filter.py
+++ filter.py
@@ -4,7 +4,7 @@ def filter_by_role(session, table_name, role):
   df = session.table(table_name)
   return df.filter(col("role") == role)
 
-def filter_by_territory(session,table_name,territory)
+def filter_by_territory(session,table_name,territory):
   df = session.table(table_name)
   return df.filter(col("territory") == territory)
 
