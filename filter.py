07206a4d4ce84a4072ff08ba62915212ef43d914
naushadhb <naushadhb@yahoo.com>
2024-09-17 08:29:12 -0400

    code fix

--- filter.py
+++ filter.py
@@ -6,9 +6,5 @@ def filter_by_role(session, table_name, role):
 
 def filter_by_territory(session,table_name,territory)
   df = session.table(table_name)
-<<<<<<< HEAD
-  return df.filter(col("territor"y) == territory)
-=======
   return df.filter(col("territory") == territory)
->>>>>>> bd5f987e94cbb32db859288766c5883f7f9c7eb9
 
