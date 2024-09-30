2ddcb0dbe06e0ea9080818fdc94d33abed78785f
naushadhb <naushadhb@yahoo.com>
2024-09-29 21:29:48 -0400

    common_class.py

--- steps/common_class.py
+++ steps/common_class.py
@@ -1,5 +1,3 @@
-import sys ,io
-import yaml
 import snowflake.snowpark as snowpark
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
@@ -9,16 +7,34 @@ import yaml
 from snowflake.snowpark.files import SnowflakeFile
 from snowflake.snowpark.functions import col
 
-class common_class(session):
-       def __init__(self,project: str, entity: str):
+class common_class():
+       config_yaml = "project_config.yml"
+       interface_name = "ENTITIES_HCP"
+       #from snowflake.snowpark import Session
+       def __init__(session,self,project: str, entity: str):
            self.project_name = project
            self.interface_name =  entity #"ENTITIES_HCP"
-           self.config_yaml = "config.yaml"
 
-       def get_unpack_sql(session,(self):
-           with open(self.config_yaml,"r") as f:
+       @staticmethod
+       def get_scope_rule_df(conf_file,session):
+           if conf_file == "ENTITIES_HCP":
+               scope_url_df = session.sql("select BUILD_SCOPED_FILE_URL(@STGS3, 'project_config.yml') as sc_url")
+           return scope_url_df
+           
+       @classmethod   
+       def get_scope_url(cls,session):
+           #select_sql = "select BUILD_SCOPED_FILE_URL({}, {}) as sc_url;".format('@STGS3',config_yaml)
+           scope_url_df = cls.get_scope_rule_df(cls.interface_name,session)
+           #scope_url = session.sql("select BUILD_SCOPED_FILE_URL(@STGS3, 'project_config.yml') as sc_url")
+           scope_url= scope_url_df.select("sc_url").collect()
+           sc_url= scope_url[0][0]
+           return sc_url
+       @classmethod
+       def get_unpack_sql(cls,session):
+           sc_url= cls.get_scope_url(session)
+           with SnowflakeFile.open(sc_url) as f:
                config_read=yaml.safe_load(f)
-               column_list = config_read["CANONICAL"][self.interface_name]
+               column_list = config_read["CANONICAL"][cls.interface_name]
                database_name = config_read["CANONICAL"]["DATABASE_NAME"]
                schema_name = config_read["CANONICAL"]["SCHEMA_NAME"]
                where_clause = ' WHERE "type"=''configuration/entityTypes/HCP'''
@@ -36,25 +52,21 @@ class common_class(session):
 
                unpack_3 = [lambda2(list(my_dec.items())[0]) for my_dec in column_list]
                unpack_3 = " AND ".join(unpack_3)
-               from_clause = "FROM {}.{}.{}_VW_STREAMS P".format(database_name, schema_name, self.interface_name)
+               from_clause = "FROM {}.{}.{}_VW_STREAMS P".format(database_name, schema_name, cls.interface_name)
 
                unpack_sql = "SELECT \n {} \n {} \n {}  \n {} \n {};".format(unpack_1, from_clause, unpack_2, where_clause,unpack_3)
            return unpack_sql
-       def get_scope_url(session,self,stg_name):
-           config_yaml=self.config_yaml
-           select_sql = "select BUILD_SCOPED_FILE_URL({}, {}) as sc_url;".format(config_yaml,stg_name)
-           scope_url = session.sql(select_sql)
-           scope_url= scope_url.select("sc_url").collect()
-           sc_url= scope_url[0][0]
-    
+       
+def main(session): 
+    # # Your code goes here, inside the "main" handler.
+    tableName = 'information_schema.packages'
+    dataframe = session.table(tableName).filter(col("language") == 'python')
 
-if __name__=="__main__":
-    with Session.builder.getOrCreate() as session:
-        calssOjbect = common_class(session,project="CANONICAL",entity="ENTITIES_HCP")
-
-        for i in range(20*5):
-            print("-",end='')
-        print("\n")
-        print(calssOjbect.get_unpack_sql(session))
-
+    # # Print a sample of the dataframe to standard output.
+    # dataframe.show()
+    calssOjbect = common_class(session,project="CANONICAL",entity="ENTITIES_HCP")
+    common_class.interface_name = "ENTITIES_HCP"
+    print(calssOjbect.get_unpack_sql(session))
+   
+    return dataframe
 
