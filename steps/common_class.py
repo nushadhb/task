dc4dc8e0d4c5d5ad924cdcbfa7722e59d45046cb
naushadhb <naushadhb@yahoo.com>
2024-09-29 21:50:10 -0400

    common_class.py

--- steps/common_class.py
+++ steps/common_class.py
@@ -1,3 +1,4 @@
+
 import snowflake.snowpark as snowpark
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
@@ -7,7 +8,7 @@ import yaml
 from snowflake.snowpark.files import SnowflakeFile
 from snowflake.snowpark.functions import col
 
-class class_common_config():
+class common_class():
        config_yaml = "project_config.yml"
        interface_name = "ENTITIES_HCP"
        #from snowflake.snowpark import Session
@@ -16,22 +17,22 @@ class class_common_config():
            self.interface_name =  entity #"ENTITIES_HCP"
 
        @staticmethod
-       def get_scope_rule_df(conf_file,session):
-           if conf_file == "ENTITIES_HCP":
+       def get_scope_rule_df(json_entity,session):
+           if json_entity == "ENTITIES_HCP":
                scope_url_df = session.sql("select BUILD_SCOPED_FILE_URL(@STGS3, 'project_config.yml') as sc_url")
            return scope_url_df
            
        @classmethod   
-       def get_scope_url(cls,session):
+       def get_scope_url(cls,session,json_entity):
            #select_sql = "select BUILD_SCOPED_FILE_URL({}, {}) as sc_url;".format('@STGS3',config_yaml)
-           scope_url_df = cls.get_scope_rule_df(cls.interface_name,session)
+           scope_url_df = cls.get_scope_rule_df(json_entity,session)
            #scope_url = session.sql("select BUILD_SCOPED_FILE_URL(@STGS3, 'project_config.yml') as sc_url")
            scope_url= scope_url_df.select("sc_url").collect()
            sc_url= scope_url[0][0]
            return sc_url
        @classmethod
-       def get_unpack_sql(cls,session):
-           sc_url= cls.get_scope_url(session)
+       def get_unpack_sql(cls,session,json_entity):
+           sc_url= cls.get_scope_url(session,json_entity)
            with SnowflakeFile.open(sc_url) as f:
                config_read=yaml.safe_load(f)
                column_list = config_read["CANONICAL"][cls.interface_name]
@@ -39,8 +40,7 @@ class class_common_config():
                schema_name = config_read["CANONICAL"]["SCHEMA_NAME"]
                where_clause = ' WHERE "type"=''configuration/entityTypes/HCP'''
 
-               lambda1 = lambda Objects: "LATERAL FLATTEN(input => ""attributes"":" + Objects[
-               0] + ", outer => true) {}".format(Objects[0])
+               lambda1 = lambda Objects: "LATERAL FLATTEN(input => ""attributes"":" + Objects[0] + ", outer => true) {}".format(Objects[0])
                lambda2 = lambda Objects: "NVL({}.value:ov::string,'true')='true'".format(Objects[0])
 
                unpack_1 = [(list(my_dec.items())[0])[0] + ".value:" + (list(my_dec.items())[0])[1] \
@@ -64,9 +64,9 @@ def main(session):
 
     # # Print a sample of the dataframe to standard output.
     # dataframe.show()
-    calssOjbect = class_common_config(session,project="CANONICAL",entity="ENTITIES_HCP")
-    class_common_config.interface_name = "ENTITIES_HCP"
-    print(calssOjbect.get_unpack_sql(session))
+    calssOjbect = common_class(session,project="CANONICAL",entity="ENTITIES_HCP")
+    common_class.interface_name = "ENTITIES_HCP"
+    print(calssOjbect.get_unpack_sql(session,'ENTITIES_HCP'))
    
     return dataframe
-
+        
