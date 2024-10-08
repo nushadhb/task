115b97ba0038c63324851483c349d07a97c8a755
naushadhb <naushadhb@yahoo.com>
2024-10-07 20:27:03 -0400

    code fix

--- steps/fileter2.py
+++ steps/fileter2.py
@@ -85,7 +85,7 @@ def get_sql(session,interface_name: str):
         schema_name = config_read["CANONICAL"]["SCHEMA_NAME"]
         where_clause = ' WHERE "type"=''configuration/entityTypes/HCP'''
 
-        confg_yaml =config_yaml_data
+        #confg_yaml =config_yaml_data
         column_list= conf_read["CANONICAL"][interface_name]
     # for my_dec in column_list:
     #     c=list(my_dec.keys())[0]
